1c3276b01c4977e29c671b39cd4dc8609c5fc409
SrjAdhikari <surajadhikari055@gmail.com>
2026-05-24 16:02:20 +0900

    test(settings): align LogoutAllDialog test with 'Log out all' label

--- tests/components/settings/LogoutAllDialog.test.tsx
+++ tests/components/settings/LogoutAllDialog.test.tsx
@@ -31,7 +31,7 @@ describe("LogoutAllDialog", () => {
 		renderWithProviders(<LogoutAllDialog onClose={vi.fn()} />);
 
 		await user.click(
-			screen.getByRole("button", { name: /log out everywhere/i }),
+			screen.getByRole("button", { name: /log out all/i }),
 		);
 
 		await waitFor(() =>
@@ -52,7 +52,7 @@ describe("LogoutAllDialog", () => {
 		renderWithProviders(<LogoutAllDialog onClose={vi.fn()} />);
 
 		await user.click(
-			screen.getByRole("button", { name: /log out everywhere/i }),
+			screen.getByRole("button", { name: /log out all/i }),
 		);
 
 		await waitFor(() =>
